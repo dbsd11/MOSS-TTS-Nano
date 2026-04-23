bfb287871ea0956c9cc0575fd646f30dfe6eeb4d
dbsd11 <bitdbsd11@163.com>
2026-04-23 21:00:20 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
-ln -s /mnt/app/models /app/models && ln -s /mnt/app/cache /app/.cache
+ln -s /mnt/app/models /app/models && ln -s /mnt/app/cache /app/.cache && ln -s /mnt/app/cache/generated_audio /app/generated_audio 
 export HF_ENDPOINT=https://hf-mirror.com
+export TMPDIR=/mnt/app/cache
+
 python app_onnx.py --host 0.0.0.0
